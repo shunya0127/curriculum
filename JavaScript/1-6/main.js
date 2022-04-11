6af7254eb2a128f738cb61097c1ef4efccf20286
shunya0127 <97280935+shunya0127@users.noreply.github.com>
2022-04-11 15:26:44 +0900

    理解度チェック

---
+++ JavaScript/1-6/main.js
@@ -0,0 +1,11 @@
+let scores = [10, 15, 20, 25];
+for (let a = a % 2 === 0; a < 4; a++) {
+    console.log(scores[a] + 'は偶数です');
+}
+
+let car = {
+    gass: 20.5,
+    num: 1234,
+};
+
+console.log(gass.num)
