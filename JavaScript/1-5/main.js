831570e632ef29ee4e06bcd9ec9e7f71455a1cf4
shunya0127 <97280935+shunya0127@users.noreply.github.com>
2022-04-11 14:48:02 +0900

    理解度チェック

--- JavaScript/1-5/main.js
+++ JavaScript/1-5/main.js
@@ -1,5 +1,5 @@
-function createejuice(fruits){
+function createjuice(fruits){
     console.log(fruits + 'を受け取りました。ジュースにして返します');
     return (fruits + 'が届きました');
 }
-createejuice('みかん');
+let orangeJuice = createjuice('みかん');
